e666362c35b901d0ff033254e11703d1fad5ced7
Derek Townsend <derekjavon2@gmail.com>
2019-04-27 16:16:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,15 +5,21 @@ end
 
 def subtraction(num1, num2)
   difference=num1-num2
+  return difference
 end
 
 def division(num1, num2)
+  quotient=num1/num2
+  return quotient
 end
 
 def multiplication(num1, num2)
+  product=num1*num2
+  return product
 end
 
 def modulo(num1, num2)
+  remainder=num1%num2
 end
 
 def square_root(num)
